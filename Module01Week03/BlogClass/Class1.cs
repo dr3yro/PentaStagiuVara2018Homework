93e9e47e987cff2ac210198ef952c973202f365e
kovacsandreitiberiu <kovacsandreitiberiu@yahoo.com>
2018-05-19 12:13:18 +0300

    Created Classes

--- Module01Week03/BlogClass/Class1.cs
+++ Module01Week03/BlogClass/Class1.cs
@@ -5,9 +5,72 @@ using System.Text;
 using System.Threading.Tasks;
 using BlogClass;
 
+/*
+    Application
+        - User
+        - Board
+        - PostMessage()
+        - CreateAccount()
+
+    User:Person
+
+       Person
+        - EMail
+        - FirstName
+        - LastName
+        - BirthDate
+
+    Post
+        - Message
+        - Author
+        - DateOfPost
+
+    Board
+        - DisplayAllPosts()
+        - OrderPostsByDateASC()
+*/
 namespace BlogClass
 {
-    public class Class1
+    public class Person
     {
+        private string FirstName;
+        private string LastName;
+        private string EMail;
+        private DateTime BrithDate;
+
+        public Person(string firstName, string lastName, string eMail, DateTime birthDate)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.EMail = eMail;
+            this.BrithDate = birthDate;
+        }
+    }
+
+    public class User : Person
+    {
+        public User(string firstName,string lastName,string eMail,DateTime birthDate) : base (firstName, lastName, eMail, birthDate)
+        {
+
+        }
+    }
+
+    public class Post
+    {
+        private string PostContent;
+        private string Author;
+        private DateTime DateOfPost;
+
+        public Post(string postContent, string author, DateTime dateOfPost)
+        {
+            this.PostContent = postContent;
+            this.Author = author;
+            this.DateOfPost = dateOfPost;
+        }
+    }
+
+    public class Board
+    {
+        
     }
 }
