52bd7e9c0d091de3fbdcac203e34f63cbc31fbc6
kovacsandreitiberiu <kovacsandreitiberiu@yahoo.com>
2018-05-30 22:20:44 +0300

    Connecting Classes

---
+++ Research/Research/Application.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Research
+{
+    class Application
+    {
+        List<object> userAccounts = new List<object>();
+        List<object> allPosts = new List<object>();
+        List<object> loggedInUser = new List<object>();
+    }
+}
