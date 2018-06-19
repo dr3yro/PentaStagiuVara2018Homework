92073f6ca71b8ec298274b03564ff6f935d7d04d
kovacsandreitiberiu <kovacsandreitiberiu@yahoo.com>
2018-06-19 07:43:01 +0300

    Remove from file logic added

--- Module02Week01/Module02Week01/RemoveFromFile.cs
+++ Module02Week01/Module02Week01/RemoveFromFile.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,26 @@ namespace Module02Week01
 {
     class RemoveFromFile
     {
+        public void removePersonAt(int index)
+        {
+            int _index = index;
+
+            using (StreamReader readFile = new StreamReader("people.txt"))
+            {
+                string line;
+                int counter = 1;
+                while ((line = readFile.ReadLine()) != null)
+                {
+                    if (counter == _index)
+                    {
+                        using (StreamWriter writeFile = new StreamWriter("people.txt"))
+                        {
+                            writeFile.WriteLine("");
+                        }
+                    }
+                    counter += 1;
+                }
+            }
+        }
     }
 }
